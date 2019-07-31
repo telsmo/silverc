f57a2d86c13b1554758d66864972bf3c93211b82
telsmo <53438575+telsmo@users.noreply.github.com>
2019-07-31 13:51:59 -0300

    Cree la sesion local de 10

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,9 +12,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(sesion_iniciada()){
+            setContentView(R.layout.mainlol);
+            String name = nom_us() + contra();
+            DatabaseHelper databaseHandler = new DatabaseHelper(this, name);
+        }else{
+            setContentView(R.layout.activity_main);
+        }
         Toolbar toolbar= findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
     public void gotomainlolinicial (View view){
 //agarro lo de user y lo pongo en la variable name, para despues crear la base con ese nombre
@@ -23,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         String user = us.getText().toString();
         String pass = ps.getText().toString();
         String name= user+pass;
-
+        //guarda en datos el user y la password
+        registrar(user,pass);
 
         setContentView(R.layout.mainlol);
         DatabaseHelper databaseHandler = new DatabaseHelper(this, name);
@@ -54,5 +63,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    public boolean sesion_iniciada() {
+        boolean a;
+        SharedPreferences prefs = getSharedPreferences("mp", MODE_PRIVATE);
+        String user = prefs.getString("user", "empty");//"empty" is the default value.
+        if (user!="empty"){
+            a=true;
+        }else{
+            a=false;
+        }
+        return a;
+    }
+    public String nom_us(){
+        SharedPreferences prefs = getSharedPreferences("mp", MODE_PRIVATE);
+        String nom = prefs.getString("user", "empty");//"No name defined" is the default value.
+        return nom;
+        }
+    public String contra(){
+        SharedPreferences prefs = getSharedPreferences("mp", MODE_PRIVATE);
+        String pass = prefs.getString("pass", "empty");//"No name defined" is the default value.
+        return pass;
+    }
+    public void registrar(String us,String pass){
+        SharedPreferences.Editor editor = getSharedPreferences("mp", MODE_PRIVATE).edit();
+        editor.putString("user", us);
+        editor.putString("pass", pass);
+        editor.apply();
+    }
 }
