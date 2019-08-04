6804a3daf4f296c2665293531c947db852377587
cetzz <53405040+cetzz@users.noreply.github.com>
2019-08-04 03:23:36 -0300

    LO ARREGLE WACHO NEVER SURRENDER LA PUT AMADRE

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -48,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
     public void gotocargar (View view){
 
         setContentView(R.layout.cargar);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        String[] categorias = new String[]{
+                "Entretenimiento", "Comida", "Transporte", "Impuestos", "Otros"};
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, categorias);
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(spinnerArrayAdapter);
 
     }
     public void cargar (View view){
