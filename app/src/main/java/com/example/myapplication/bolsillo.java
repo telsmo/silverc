39e45a2d863fd4b67b83a4aa650db888882fc242
cetzz <53405040+cetzz@users.noreply.github.com>
2019-08-08 20:18:29 -0300

    lol

--- app/src/main/java/com/example/myapplication/bolsillo.java
+++ app/src/main/java/com/example/myapplication/bolsillo.java
@@ -7,21 +7,30 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class bolsillo extends Activity {
     private DatabaseHelper databaseHandler;
-    Intent intent = getIntent();
-    String name = intent.getExtras().getString("nombre");
+    private String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        name = intent.getExtras().getString("nombre");
         databaseHandler = new DatabaseHelper(this, name);
         setContentView(R.layout.bolsillo);
     }
 
     public void gotocrearbol (View view){
-        Intent bol = new Intent(this,CargarBolsillo.class);
-        bol.putExtra("nombre",name);
-        startActivity(bol);
+        setContentView(R.layout.crearbolsillo);
+    }
+    public void agregar_bol (View view){
+        final EditText b1 = (EditText) findViewById(R.id.cb1);
+        final EditText b2 = (EditText) findViewById(R.id.cb2);
+        String cb1 = b1.getText().toString();
+        String cb2 = b2.getText().toString();
+        long result= databaseHandler.addBol("a",2);
+        Toast.makeText(this,Long.toString(result),Toast.LENGTH_LONG).show();
+        setContentView(R.layout.bolsillo);;
     }
 }
